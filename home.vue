4d976b22d67d984fe42aca14cc11544e8df8ba84
luoyu <35693463+luoyu6@users.noreply.github.com>
2019-03-28 17:00:50 +0800

    Create home.vue

---
+++ home.vue
@@ -0,0 +1,29 @@
+<template>
+	<div class="index">
+	<!--导航分类-->
+		<navBar></navBar>
+	</div>
+</template>
+
+<script>
+	import navBar from '@/components/navBar'
+	export default {
+		name: 'HelloWorld',
+		data() {
+			return {
+				selected:[],
+			}
+		},
+
+		components: {
+			navBar
+			
+		}
+	}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+
+<style lang="stylus">
+
+</style>
