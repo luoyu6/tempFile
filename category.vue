b56bdb2bf8d25e8ca94066a4515dc6a6e6aa4a6b
luoyu <35693463+luoyu6@users.noreply.github.com>
2019-03-28 17:00:04 +0800

    Create category.vue

---
+++ category.vue
@@ -0,0 +1,44 @@
+<template>
+	<!--选项-->
+    <div class="category">
+        <ul class="clearfix">
+            <li :style = "widthClass" v-for="item1 in argObj" class="fl"><router-link href="#" :to='item1.path'><img :src='item1.url' alt=""></router-link></li>     
+        </ul>
+       
+    </div>
+</template>
+<script>
+export default {
+name: 'HelloWorld',
+data () {
+    return {    
+    }
+},
+  props:["argObj","widthClass"]
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+	.clearfix:after{
+		clear: both;
+		content: '';
+		display: block;
+		height: 0;
+		visibility: hidden;
+	}
+	.fl{
+		float: left;
+	}
+	.fr{
+		float: right;
+	}
+	ul li{
+		list-style: none;
+	}
+
+
+.category ul li img{
+	width: 100%;
+}
+</style>
